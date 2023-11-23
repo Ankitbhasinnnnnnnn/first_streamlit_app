a5e222c41315bd7f67513833def29a95f205e502
Ankitbhasinnnnnnnn <105282765+Ankitbhasinnnnnnnn@users.noreply.github.com>
2023-11-23 12:23:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.title('My Parents New Healthy Diner')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.multiselect("Pick Some fruits:" , list(my_fruit_list.index),['Avocado', 'Strawberries']))
+streamlit.multiselect("Pick Some fruits:" , list(my_fruit_list.index),['Avocado', 'Strawberries'])
 
 
 
