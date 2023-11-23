012e5d2e6418a9b6685e8f85ed83d3431792c8f3
Ankitbhasinnnnnnnn <105282765+Ankitbhasinnnnnnnn@users.noreply.github.com>
2023-11-23 12:20:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,8 @@ streamlit.text('Hard-boiled Free-Range Egg')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.multiselect("Pick Some fruits:" , list(my_fruit_list.index))
+streamlit.multiselect("Pick Some fruits:" , list(my_fruit_list.index)),['Avocado', 'Strawberries'])
+
 
 
 streamlit.dataframe(my_fruit_list)
