14b17dcce4e7b93316b34e77772ce60735cafaa3
Ankitbhasinnnnnnnn <105282765+Ankitbhasinnnnnnnn@users.noreply.github.com>
2023-11-23 12:38:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,20 @@
-
-import pandas;
-import streamlit;
-
-streamlit.title('My Parents New Healthy Diner') 
-
+import streamlit ;
+import pandas ;
+streamlit.title('My Parents New Healthier Diner')
+ 
+streamlit.header('Breakfast')
+streamlit.text(' 🥣 Omega 3 & blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+ 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+ 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')
+ 
+fruit_selected =streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruit_to_show=my_fruit_list.loc[fruit_selected]
+ 
+ 
+streamlit.dataframe(fruit_to_show)
